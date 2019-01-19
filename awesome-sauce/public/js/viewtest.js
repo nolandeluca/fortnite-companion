9641632ea2e0efb26f5d24e809b33b0550ca4e67
unknown <godly.mg2@gmail.com>
2019-01-19 14:17:16 -0500

    This is only a working copy with limited front-end and we have to add firebase logic to it

--- awesome-sauce/public/js/viewtest.js
+++ awesome-sauce/public/js/viewtest.js
@@ -3,39 +3,78 @@
 //     method: "GET"
 //   }).then(function(response) {
 //       console.log(response)
-     
+
 //   });
-$(document).ready(function() {
+$(document).ready(function () {
   $(".todo-container").empty();
   setTimeout(getTodos, 100);
   function getTodos() {
-  $.get("/api/stats", function(data) {
-    console.log(data)
-    for(i=0;i<data.length;i++){
-    $(".todo-container").prepend('<p>'+data[i].userid+'</p>');
+    $.get("/api/stats", function (data) {
+      console.log(data)
+      for (i = 0; i < data.length; i++) {
+        $(".todo-container").prepend('<p>' + data[i].userid + '</p>');
+      }
+    });
   }
-  });
-}
 
-//getTodos();
-$("#save").on("click",postTodos);
+  //getTodos();
+  $("#save").on("click", postTodos);
 
 
-function postTodos(){
-  event.preventDefault();
-  getTodos();
-  var todo = {
+  function postTodos() {
+    event.preventDefault();
+    checkDuplicates();
+    var todo = {
       userid: $("#userid").val(),
       kills: $("#kills").val(),
-      wins:$("#wins").val(),
-      matchesplayed:$("#matches_played").val(),
-      kd:$("#kd").val()
-  };
-    $.post("/api/stats",todo,function(dbStat){
+      wins: $("#wins").val(),
+      matchesplayed: $("#matches_played").val(),
+      kd: $("#kd").val()
+     
+    };
+    $.post("/api/stats", todo, function (dbStat) {
       console.log(dbStat);
-    
+      $(".todo-container").empty(); 
+      //checkDuplicates();
     });
-}
+  }
 
+
+  function checkDuplicates() {
+    var todo = {
+      userid: $("#userid").val(),
+      kills: $("#kills").val(),
+      wins: $("#wins").val(),
+      matchesplayed: $("#matches_played").val(),
+      kd: $("#kd").val()
+     
+    };
+    $.get("/api/stats", function (data) {
+      for (i = 0; i < data.length; i++) {
+        console.log(data[i].username);
+        if(data[i].username=$("#userid").val())
+        {
+          
+          updateStat(todo);
+          console.log('Already Exists')
+        }
+        else{
+          getTodos();
+        }
+      }
+    });
+  }
+
+  function updateStat(todo) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/stats",
+      data:todo
+      
+    })
+      .then(function() {
+       console.log('Added');
+      });
+  }
 
 });
