7e5d53e9ec4877bd75a42ca97ff4b7e0548641ad
nolandeluca <delucanolan@gmail.com>
2019-01-22 19:54:47 -0500

    cleaning up html

--- awesome-sauce/public/js/logic.js
+++ awesome-sauce/public/js/logic.js
@@ -1,10 +1,12 @@
+src="apiRoutes.js"
+
 $(document).ready(function () {
     $('.modal').modal();
     firebase.auth().signOut();
 });
-$(document).ready(function () {
+$(document).ready(function(){
     $('select').formSelect();
-});
+  });
 
 // Initialize Firebase
 var config = {
@@ -14,8 +16,8 @@ var config = {
     projectId: "project2-7c927",
     storageBucket: "project2-7c927.appspot.com",
     messagingSenderId: "40852562411"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
 
 const txtEmail = document.getElementById('email');
@@ -54,12 +56,12 @@ btnLogout.addEventListener('click', e => {
 });
 
 //User dependant functions
-var user;
-var firebaseUser;
-var email, uid;
-firebase.auth().onAuthStateChanged(function (user) {
+    var user;
+    var firebaseUser;
+    var email, uid;
+firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        // User is signed in.
+      // User is signed in.
         loggedIn = true;
         btnLogout.classList.remove('hide');
         btnLogin1.classList.add('hide');
@@ -88,18 +90,18 @@ firebase.auth().onAuthStateChanged(function (user) {
             };
         });
     } else {
-        // User is signed out.
+      // User is signed out.
         loggedIn = false;
         btnLogout.classList.add('hide');
         btnLogin1.classList.remove('hide');
         $("#welcome").text(" ");
     }
-});
+  });
 
-//Storing event in firebase
+  //Storing event in firebase
+    
 
-
-$(document).on("click", ".favorite", function () {
+  $(document).on("click", ".favorite", function () {
     if (loggedIn) {
         database.ref().child('users').child(uid).set({
             email: user.email,
@@ -127,3 +129,93 @@ $(document).on("click", ".favorite", function () {
         this.className = "btn disabled";
     };
 });
+
+//-----------------------------FRIENDFINDER-----------------------------
+
+var i = 0;
+    var txt = 'Fornite Companion.';
+    var speed = 50;
+    var user_id;
+    var killStats = [];
+
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("demo").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+    //typeWriter()
+
+
+    $("#stat").on('click', function (e) {
+        //delucanolan
+        e.preventDefault();
+        var username = $("#userid").val().trim();
+        var queryURL = "https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=" + username;
+        var html = "";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.uid)
+            console.log(response.platforms[1])
+            //$("#userid").val(response.uid);
+            user_id = response.uid;
+            setTimeout(showstats, 3);
+        });
+    })
+    
+
+    function showstats() {
+        var platform1 = $("#platform1").val();
+        var queryURL = "https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats?user_id=" + user_id + "&platform=" + platform1;
+        var html = "";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            //  console.log(response.platforms[1])
+
+
+            $("#kills").val(response.totals.kills);
+            $("#wins").val(response.totals.wins);
+            $("#matches_played").val(response.totals.matchesplayed);
+            $("#kd").val(response.totals.kd);
+            $("#score_solo").val(response.stats.score_solo);
+            killStats.push(response.stats.kills_solo, response.stats.kills_duo, response.stats.kills_squad)
+            console.log("statsssss" + Stat);
+            
+
+            //#######            CHART
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'line',
+
+                // The data for our dataset
+                data: {
+                    labels: ["Solo Kills", "Duo Kills", "Kill Squad"],
+                    datasets: [{
+                        label: "Kills",
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: killStats,
+                    }]
+                },
+
+                // Configuration options go here
+                options: {}
+            });
+
+            //CHART END
+
+        });
+
+    }
+
+
+
+
